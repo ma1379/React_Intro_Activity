de565f22445b168d3796935fffeb691f267798a8
Brandon Moyer <etaleocallidum@gmail.com>
2022-06-16 14:39:34 -0500

    part 4: a new hope
    i love retroactively changing titles

--- 01-starting-setup/01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/01-starting-setup/src/components/ExpenseItem.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import "./ExpenseItem.css";
-function ExpenseItem() {
-    return (
-        <div className='expense-item'>
-           <div>June 10th 2022</div> 
-           <div className='expense-item__description'>
-            <h2>Car Insurance</h2>
-            <div className='expense-item__price'>$294.67</div>
-           </div>
+function ExpenseItem(props) {
+    const month = props.date.toLocaleString('en-us', {month: 'long'})
+    const day = props.date.toLocaleString('en-us', {day: '2-digit'})
+    const year = props.date.getFullYear();
+    return(
+    <div className='expense-item'>
+        <div>{props.date.toISOString()}</div>
+        <div className='expense-item__description'>
+            <h2>{props.title}</h2>
+            <div className='expense-item__price'>${props.amount}</div>
         </div>
+    </div>
     );
 }
 export default ExpenseItem;
