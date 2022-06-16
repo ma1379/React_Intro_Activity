679838ea72aa0cbdccdc75d750d5deeedbe5710a
Surya Arvind <24arvins@elmbrookstudents.org>
2022-06-16 13:52:12 -0500

    Part 3 of React

--- 01-starting-setup/01-starting-setup/src/App.js
+++ 01-starting-setup/01-starting-setup/src/App.js
@@ -1,8 +1,11 @@
+import React from "react";
+import ExpenseItem from "./components/ExpenseItem";
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
       <p>This is also visible.</p>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
