1062f39093f530952a5fbb86b232369978ec3b95
Sanjay Murali <muralisanjay961@gmail.com>
2022-06-16 14:39:33 -0500

    saving changes

--- 01-starting-setup/01-starting-setup/components/ExpenseItem.js
+++ 01-starting-setup/01-starting-setup/components/ExpenseItem.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import "./ExpenseItem.css";
-function ExpenseItem() {
+function ExpenseItem(props) {
+    const month = props.date.toLocalString('en-US', {month = 'long'})
+    const day = props.date.toLocalString('en-US', {day = '2-digit'})
+    const year = props.date.getFullYear();
     return (
         <div className = "expense-item">
-        <div>June 10th 2022</div>
-        <div className = "expense-item__description">
-            <h2>Car Insurance</h2>
-            <div "expense-item__price">$294.67</div>
+        <div>{props.date.toISOString()}</div>
+       <div className = "expense-item__description">
+         <h2>{props.title}</h2>
+       <div "expense-item__price">${props.amount}</div>
         </div>
         </div>
-    );
+      );
+      
 }
 export default ExpenseItem;
