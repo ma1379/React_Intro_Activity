06746ab0a63a4a33887758209f227c5402644952
Mitchell <krasma423@gmail.com>
2022-06-16 13:01:58 -0500

    Added ExpenseItem CSS

--- 01-starting-setup/01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/01-starting-setup/src/components/ExpenseItem.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import "./ExpenseItem.css"
+
 function ExpenseItem() {
     return (
-        <div><h2>Expense Item!</h2></div>
+    <div className='expense-item'>
+        <div>June 10th 2022</div>
+        <div className='expense-item__description'>
+            <h2>Expense Item!</h2>
+            <div className='expense-item_price'>$294.67</div>
+        </div>
+    </div>
     );
 }
 
