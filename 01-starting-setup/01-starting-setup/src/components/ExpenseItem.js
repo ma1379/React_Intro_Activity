644f26fd07d06d2bbe011b51624a64055a9c1fce
Puj Manohar <23manohp@elmbrookstudents.org>
2022-06-16 14:19:27 -0500

    Part 3 Complete

--- 01-starting-setup/01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/01-starting-setup/src/components/ExpenseItem.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import "./ExpenseItem.css";
 
-function ExpenseItem(){
+function ExpenseItem(props) {
     return (
-             <div className='expensive-item' >
-                <div>June 10th 2022</div>
-             <div className='expensive-item__description'><h2>Car Insurance</h2>
-             <div className='expensive-item__price'>$294.67</div>
-             </div>
-             </div>
-
+    <div className='expense-item'>
+        <div>{props.date.toISOString()}</div>
+        <div className='expense-item__description'>
+            <h2>{props.title}</h2>
+            <div className='expense-item__price'>{props.amount}</div>
+        </div>
+    </div>
     );
 }
+
 
 export default ExpenseItem;
