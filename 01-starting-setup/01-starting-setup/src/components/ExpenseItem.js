f0e7bf0f02dabae94042e1d7932c1fbdd55cec99
Surya Arvind <24arvins@elmbrookstudents.org>
2022-06-16 13:03:48 -0500

    React continued

--- 01-starting-setup/01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/01-starting-setup/src/components/ExpenseItem.js
@@ -1,7 +1,14 @@
 import React from "react";
+import "./ExpenseItem.css";
 function ExpenseItem() {
     return(
-        <div><h2>ExpenseItem!</h2></div>
+        <div className='expense-item'>
+           <div>June 10th 2022</div> 
+           <div className='expense-item__description'>
+             <h2>Car Insurance</h2>
+             <div className='expense-item__price'>$294.67</div>
+           </div>
+        </div>
     );
 }
 export default ExpenseItem;
