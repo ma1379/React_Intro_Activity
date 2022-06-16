1ab5674d81eff6afee784fb321b5dd32a09c79ab
Brandon Moyer <etaleocallidum@gmail.com>
2022-06-16 13:02:22 -0500

    part 2 complete

--- 01-starting-setup/01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/01-starting-setup/src/components/ExpenseItem.js
@@ -1,8 +1,14 @@
 import React from 'react';
-
+import "./ExpenseItem.css";
 function ExpenseItem() {
     return (
-        <div><h2>Expense Item!</h2></div>
+        <div className='expense-item'>
+           <div>June 10th 2022</div> 
+           <div className='expense-item__description'>
+            <h2>Car Insurance</h2>
+            <div className='expense-item__price'>$294.67</div>
+           </div>
+        </div>
     );
 }
-export default ExpenseItem
+export default ExpenseItem;
