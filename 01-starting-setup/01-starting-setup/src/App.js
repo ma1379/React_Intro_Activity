6fbf0d014974d266d4b57a2fa5ec913ffe788f57
Brandon Moyer <etaleocallidum@gmail.com>
2022-06-16 13:37:49 -0500

    part 3
    this is the program that never ends
    oh it goes on and on my friends

--- 01-starting-setup/01-starting-setup/src/App.js
+++ 01-starting-setup/01-starting-setup/src/App.js
@@ -1,8 +1,12 @@
+import React from 'react';
+import ExpenseItem from "./components/ExpenseItem.js";
+
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
       <p> This is also visible</p>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
