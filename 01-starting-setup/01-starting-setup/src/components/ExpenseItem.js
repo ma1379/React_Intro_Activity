cc8b6e3624096c7d8f57c4b2c3fc192aa359890d
Mitchell <krasma423@gmail.com>
2022-06-16 14:39:18 -0500

    Almost Done with Date Formatting

--- 01-starting-setup/01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/01-starting-setup/src/components/ExpenseItem.js
@@ -2,6 +2,10 @@ import React from 'react';
 import "./ExpenseItem.css"
 
 function ExpenseItem(props) {
+    const month = props.date.toLocaleString('en-US', {month: 'long'});
+    const day = props.date.toLocaleString('en-US', {day: '2-digit'});
+    const year = props.date.getFullYear();
+
     return (
     <div className='expense-item'>
         <div>{props.date.toISOString()}</div>
