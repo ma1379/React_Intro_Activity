723839821c0874c26b7cfeaeeb571e4be17d0d74
Puj Manohar <23manohp@elmbrookstudents.org>
2022-06-16 13:01:52 -0500

    Part 2 Complete

--- 01-starting-setup/01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/01-starting-setup/src/components/ExpenseItem.js
@@ -1,8 +1,14 @@
 import React from 'React'
+import "./ExpenseItem.css"
 
 function ExpenseItem(){
     return (
-             <div><h2>Expense Item!</h2></div>
+             <div className='expensive-item' >
+                <div>June 10th 2022</div>
+             <div className='expensive-item__description'><h2>Car Insurance</h2>
+             <div className='expensive-item__price'>$294.67</div>
+             </div>
+             </div>
 
     );
 }
