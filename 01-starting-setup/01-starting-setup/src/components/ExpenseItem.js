4f3c07a5e94d7b65b6b22d56a3d27fefaeb9ef9c
Puj Manohar <23manohp@elmbrookstudents.org>
2022-06-17 09:55:16 -0500

    Part 5 Done

--- 01-starting-setup/01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/01-starting-setup/src/components/ExpenseItem.js
@@ -1,22 +1,17 @@
 import React from 'react';
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate.js";
+
 
 function ExpenseItem(props) {
-    const month = props.date.toLocaleString('en-US',{month:'long'});
-    const day = props.date.toLocaleString('en-US',{day:'2-digit'});
-    const year = props.date.getFullYear();
+    
     return (
-    <div className='expense-item'>
-        <div>
-        <div>{month}</div>
-        <div>{day}</div>
-        <div>{year}</div>
-        </div>
-        <div className='expense-item__description'>
+        <div className='expense-item'>
+            <ExpenseDate date = {props.date}></ExpenseDate>
+            <div className='expense-item__description'></div>
             <h2>{props.title}</h2>
-            <div className='expense-item__price'>{props.amount}</div>
+            <div className = 'expense-item__price'>${props.amount}</div>
         </div>
-    </div>
     );
 }
 
