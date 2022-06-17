7d9363421f4faa8397212c1601a2b566b2fe6e1e
Brandon Moyer <etaleocallidum@gmail.com>
2022-06-17 09:27:37 -0500

    part 5: the empire strikes back

--- 01-starting-setup/01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/01-starting-setup/src/components/ExpenseItem.js
@@ -5,8 +5,13 @@ function ExpenseItem(props) {
     const day = props.date.toLocaleString('en-us', {day: '2-digit'})
     const year = props.date.getFullYear();
     return(
+      
     <div className='expense-item'>
-        <div>{props.date.toISOString()}</div>
+        <div>
+            <div>{month}</div>
+            <div>{day}</div>
+            <div>{year}</div>
+        </div> 
         <div className='expense-item__description'>
             <h2>{props.title}</h2>
             <div className='expense-item__price'>${props.amount}</div>
