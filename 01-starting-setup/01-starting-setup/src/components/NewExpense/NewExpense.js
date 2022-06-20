6a818fb17141973eb9e75c6dd39123aef3f9aa7e
Surya Arvind <24arvins@elmbrookstudents.org>
2022-06-20 12:22:16 -0500

    Built saveExpenseDataHandler

--- 01-starting-setup/01-starting-setup/src/components/NewExpense/NewExpense.js
+++ 01-starting-setup/01-starting-setup/src/components/NewExpense/NewExpense.js
@@ -2,9 +2,16 @@ import React from "react";
 import './NewExpense.css'
 import ExpenseForm from "./ExpenseForm";
 function NewExpense() {
+  function saveExpenseDataHandler(enteredExpenseData){
+  const expenseData={
+    ...enteredExpenseData,
+    id:Math.random().toString()
+  }
+  console.log(expenseData)
+  }
     return(
         <div className='new-expense'>
-            <ExpenseForm></ExpenseForm>
+            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}></ExpenseForm>
         </div>
     );
 }
