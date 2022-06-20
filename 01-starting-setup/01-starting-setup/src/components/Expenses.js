0dde9c127aaef6b952be0719ea4114d6ab5ff881
Puj Manohar <23manohp@elmbrookstudents.org>
2022-06-20 13:19:09 -0500

    Year Drop Down Done

--- 01-starting-setup/01-starting-setup/src/components/Expenses.js
+++ 01-starting-setup/01-starting-setup/src/components/Expenses.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import Card from './Card.js';
+import ExpenseFilter from "./newExpense/ExpensesFilter.js";
 
 
 function Expenses (props){
+    const[filteredYear , setFilteredYear] = useState("2020");
+    function filterChangeHandler (selectedYear){
+        setFilteredYear(selectedYear);;
+        console.log("In Expenses");
+        console.log(selectedYear);
+    }
     return (
  <Card className = "expenses">
+    <ExpenseFilter selected = {filteredYear} onFilterChange ={filterChangeHandler}></ExpenseFilter>
      <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date}></ExpenseItem>
      <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date}></ExpenseItem>
      <ExpenseItem title={props.items[2].title} amount={props.items[2].amount} date={props.items[2].date}></ExpenseItem>
