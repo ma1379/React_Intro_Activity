f977aa90cb420c69ae9cd4da009958d950ebcda5
Puj Manohar <23manohp@elmbrookstudents.org>
2022-06-20 14:13:27 -0500

    Functions blah blah

--- 01-starting-setup/01-starting-setup/src/components/Expenses.js
+++ 01-starting-setup/01-starting-setup/src/components/Expenses.js
@@ -8,14 +8,17 @@ import ExpenseFilter from "./newExpense/ExpensesFilter.js";
 function Expenses (props){
     const[filteredYear , setFilteredYear] = useState("2020");
     function filterChangeHandler (selectedYear){
-        setFilteredYear(selectedYear);;
-        console.log("In Expenses");
-        console.log(selectedYear);
+        setFilteredYear(selectedYear);
     }
+    
+    const filteredExpenses = props.items.filter(expenses =>{
+        return expenses.date.getFullYear().toString() === filteredYear;
+      });
+
     return (
  <Card className = "expenses">
     <ExpenseFilter selected = {filteredYear} onFilterChange ={filterChangeHandler}></ExpenseFilter>
-    {props.items.map((expensesElement) => (
+    {filteredExpenses.map((expensesElement) => (
         <ExpenseItem
           key = {expensesElement.id}
           title={expensesElement.title}
