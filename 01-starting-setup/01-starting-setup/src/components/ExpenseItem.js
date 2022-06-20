02e6ee8df8fdbf1cbdd157a12c64fb852057cc3f
Brandon Moyer <etaleocallidum@gmail.com>
2022-06-20 09:43:09 -0500

    added buttons

--- 01-starting-setup/01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/01-starting-setup/src/components/ExpenseItem.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate.js";
 import Card from "./Card.js";
 function ExpenseItem(props) {
-
+    const [title, setTitle] = useState(props.Title);
+    function ClickHandler() {
+        setTitle('Update');
+    }
     return(
     <Card className='expense-item'>
         <ExpenseDate date={props.date}></ExpenseDate>
@@ -11,6 +14,7 @@ function ExpenseItem(props) {
             <h2>{props.title}</h2>
             <div className='expense-item__price'>${props.amount}</div>
         </div>
+        <button onClick={ClickHandler}>Change Title</button>
     </Card>
     );
 }
