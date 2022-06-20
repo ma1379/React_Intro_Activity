f42042dfd79fe9efb7fd0c08df58a07556ba7cd3
Mitchell <krasma423@gmail.com>
2022-06-20 13:59:38 -0500

    Adding to list of Expenses

--- 01-starting-setup/01-starting-setup/src/components/Expenses.js
+++ 01-starting-setup/01-starting-setup/src/components/Expenses.js
@@ -14,10 +14,14 @@ function Expenses(props) {
     return (
     <Card className="expenses">
       <ExpensesFilter selected={filteredYear} onFilterChange={filterChangeHandler}></ExpensesFilter>
-      <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date}></ExpenseItem>
-      <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date}></ExpenseItem>
-      <ExpenseItem title={props.items[2].title} amount={props.items[2].amount} date={props.items[2].date}></ExpenseItem>
-      <ExpenseItem title={props.items[3].title} amount={props.items[3].amount} date={props.items[3].date}></ExpenseItem>
+      {props.items.map((expensesElement) => (
+        <ExpenseItem
+          key={expensesElement.id}
+          title={expensesElement.title}
+          amount={expensesElement.amount}
+          date={expensesElement.date}
+        ></ExpenseItem>
+      ))}
     </Card>
   );
 }
