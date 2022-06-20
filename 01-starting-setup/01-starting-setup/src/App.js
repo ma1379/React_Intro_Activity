938894bc953ab426f048a7658658e8d99a9367c0
Brandon Moyer <etaleocallidum@gmail.com>
2022-06-20 10:40:14 -0500

    blood, sweat, and new expenses

--- 01-starting-setup/01-starting-setup/src/App.js
+++ 01-starting-setup/01-starting-setup/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Expenses from './components/Expenses.js';
+import NewExpense from './components/newExpense/NewExpense.js';
 function App() {
   const expenses = [
     {
@@ -24,7 +25,8 @@ function App() {
   ];
   return(
     <div>
-      <h2>Let's get started!</h2>
+      
+      <NewExpense>some stuff</NewExpense>
       <Expenses items={expenses}></Expenses>
     </div>
   );
--- 01-starting-setup/01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/01-starting-setup/src/components/ExpenseItem.js
@@ -3,7 +3,7 @@ import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate.js";
 import Card from "./Card.js";
 function ExpenseItem(props) {
-    const [title, setTitle] = useState(props.Title);
+    const [title, setTitle] = useState(props.title);
     function ClickHandler() {
         setTitle('Update');
     }
@@ -11,7 +11,7 @@ function ExpenseItem(props) {
     <Card className='expense-item'>
         <ExpenseDate date={props.date}></ExpenseDate>
         <div className='expense-item__description'>
-            <h2>{props.title}</h2>
+            <h2>{title}</h2>
             <div className='expense-item__price'>${props.amount}</div>
         </div>
         <button onClick={ClickHandler}>Change Title</button>
