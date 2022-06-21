dda92a21a07cf77756a071060e8a3a2fdbfb87aa
Sanjay Murali <muralisanjay961@gmail.com>
2022-06-21 09:33:22 -0500

    done

--- 01-starting-setup/01-starting-setup/src/App.js
+++ 01-starting-setup/01-starting-setup/src/App.js
@@ -1,38 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Expenses from "./components/Expenses";
 import newExpense from './components/newExpense/newExpense.js';
+const InitialExpenses = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
 function App() {
+  const [expenses, setExpenses] = useState(InitialExpenses);
   function addExpenseHandler(expense) {
-    console.log("In app.js")
-    console.log(expense)
-  }
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+    setExpenses((prevExpenses) => {
+      return [...prevExpenses, expense];
+    });
   return (
     <div>
       <newExpense onAddExpense = {addExpenseHandler}></newExpense>
       <Expenses items = {expenses}></Expenses>
      </div>
   );
+}
 }
 
 export default App;
--- 01-starting-setup/01-starting-setup/src/components/newExpense/ExpenseForm.js
+++ 01-starting-setup/01-starting-setup/src/components/newExpense/ExpenseForm.js
@@ -1,10 +1,10 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import "./ExpenseForm.css";
 
 function ExpenseForm(props) {
-    const[enteredTitle, setEnteredTitle] = useState("");
-    const[enteredAmount, setEnteredAmount] = useState("");
-    const[enteredDate, setEnteredDate] = useState("");
+    const [enteredTitle, setEnteredTitle] = useState("");
+    const [enteredAmount, setEnteredAmount] = useState("");
+    const [enteredDate, setEnteredDate] = useState("");
     function titleChangeHandler(event) {
         setEnteredTitle(event.target.value);
     }
@@ -27,23 +27,24 @@ function ExpenseForm(props) {
         setEnteredTitle('');
     };
     return (
-        <form onSubmit = {submitHandler}>
+        <form onSubmit={submitHandler}>
             <div className="new-expense__controls">
                 <div className="new-expense__controls">
                     <label>Title</label>
-                    <input type="text" value = {enteredTitle} onChange = {titleChangeHandler}></input>
+                    <input type="text" value={enteredTitle} onChange={titleChangeHandler}></input>
                 </div>
                 <div className="new-expense__controls">
                     <label>Amount</label>
-                    <input type="number" min="0.01" step="0.01" value = {enteredAmount} onChange = {amountChangeHandler}></input>
+                    <input type="number" min="0.01" step="0.01" value={enteredAmount} onChange={amountChangeHandler}></input>
                 </div>
                 <div className="new-expense__controls">
                     <label>Date</label>
-                    <input type="date" min="2019-01-01" max="2022-12-31" value = {enteredDate} onChange = {dateChangeHandler}></input>
+                    <input type="date" min="2019-01-01" max="2022-12-31" value={enteredDate} onChange={dateChangeHandler}></input>
                 </div>
             </div>
-            <div className = 'new-expense__actions'>
-                <button type = 'submit'>Add Expense</button>
+            <div className='new-expense__actions'>
+                <button type="button" onClick={props.onCancel}>Cancel</button>
+                <button type='submit'>Add Expense</button>
             </div>
         </form>
     );
