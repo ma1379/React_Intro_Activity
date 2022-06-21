41f7487776533e1592933fafd7c77fcc2b0885e0
Puj Manohar <23manohp@elmbrookstudents.org>
2022-06-21 08:20:06 -0500

    Individual Activity

--- 01-starting-setup/01-starting-setup/src/components/newExpense/NewExpense.js
+++ 01-starting-setup/01-starting-setup/src/components/newExpense/NewExpense.js
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './NewExpense.css';
 import ExpenseForm from './ExpenseForm.js';
 
 function NewExpense(props){
+    const [isEditing, setIsEditing] = useState(false);
     function saveExpenseDataHandler (enteredExpenseData){
           const expenseData = { ...enteredExpenseData, id: Math.random().toString() }
         props.onAddExpense(expenseData);
+        setIsEditing(false);
+
     };
 
-    return(
-            <div className = 'new-expense'>
-            <ExpenseForm onSaveExpenseData ={saveExpenseDataHandler}></ExpenseForm>
+    function startEditingHandler() {
+        setIsEditing(true);
+      }
 
-</div>
+      function stopEditingHandler() {
+        setIsEditing(false);
+      }
+    
+    
+    return(
+        <div className="new-expense">
+      {isEditing===false && <button onClick={startEditingHandler}>Add New Expense</button>}
+      {isEditing===true && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancel={stopEditingHandler}></ExpenseForm>}
+    </div>
 
     );
 }
